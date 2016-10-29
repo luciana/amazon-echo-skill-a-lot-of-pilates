f9a631fae5945141fc3c249d4435ccf3ee3f9b5f
Luciana Bruscino <luciana.bruscino@gmail.com>
2016-10-29 00:48:36 -0400

    more merge changes

--- src/config.js
+++ src/config.js
@@ -2,13 +2,11 @@ var config = {};
 
 config.api_key = 'be65fe76147759d7931718826f7db263';
 config.app_id = 'amzn1.echo-sdk-ams.app.ef7b5d42-f176-4806-9ea3-6ef6d041c2aa';
-<<<<<<< HEAD
 config.api_secret = 'MPP-Allow-API-Call';
 config.client_id ='alop-alexa-skill';
 config.host_name = 'www.alotofpilates.com';
-=======
 config.host_name ='www.alotofpilates.com';
 config.api_secret ='MPP-Allow-API-Call';
->>>>>>> linking
+
 
 module.exports = config;
