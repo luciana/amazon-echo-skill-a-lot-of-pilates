e9e596ee8a8bb587cd2b6c445fa8d2a9b8ee0c9b
Luciana Bruscino <luciana.bruscino@gmail.com>
2017-03-13 22:21:14 -0400

    checking application id

--- src/config.js
+++ src/config.js
@@ -2,7 +2,8 @@ var config = {};
 
 config.api_key = 'be65fe76147759d7931718826f7db263';
 
-config.app_id = 'amzn1.echo-sdk-ams.app.ef7b5d42-f176-4806-9ea3-6ef6d041c2aa';
+//config.app_id = 'amzn1.echo-sdk-ams.app.ef7b5d42-f176-4806-9ea3-6ef6d041c2aa';
+config.app_id ='amzn1.ask.skill.8a6e07e0-d6c4-4c24-9b37-b63245116251';
 if (process.env.NODE_ENV == 'dev'){
 	config.app_id ='amzn1.ask.skill.8a6e07e0-d6c4-4c24-9b37-b63245116251';
 }
