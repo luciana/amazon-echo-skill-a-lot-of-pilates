a781b02eab222746f48a9ce240ec9f18dad626c6
Luciana Bruscino <luciana.bruscino@gmail.com>
2021-02-24 12:29:11 -0500

    added portuguese localization for some of the speech text

--- ALOPPilatesClass/lambda/i18n/pt.js
+++ ALOPPilatesClass/lambda/i18n/pt.js
@@ -8,32 +8,32 @@ module.exports = {
       "Coloca o seu mat no chão. " +
       ".<break time=\"1.5s\" /> " +
       "Você esta pronta pra começar a aula?",
-      WELCOME_REPROMPT: "I can lead you through a pilates sequence " +
+      WELCOME_REPROMPT: "Eu posso ajudar com uma sequência de exercícios pilates " +
       "<break time=\"0.2s\" /> " +
-      " You can also "+
-      " visit ALotOfPilates.com and take a video instructed class. " +
+      " Você também pode "+
+      " visitar ALotOfPilates.com para fazer outras aulas. " +
       ".<break time=\"0.7s\" /> " +
-      "Just say start class when ready. Should I start?",
-      STARTOVER_MESSAGE: "I can lead you through a pilates sequence Or you can say exit. Do you want to start the class?",
-      STARTOVER_REPROMPT: "Do you want to start the class?",    
-      GET_START_EXERCISE_MESSAGE: ["Get ready on your mat for the %s", "Let's get started with  %s"],
-      GET_NEXT_EXERCISE_MESSAGE: ["Next exercise is  %s", "Moving on to the %s", "Next is  %s"],
-      GET_START_EXERCISE_DEFAULT_MESSAGE:"Get ready on your mat to get started",
-      GET_NEXT_EXERCISE_DEFAULT_MESSAGE: "Moving on to the next exercise",
-      TEACH_CLASS_DONE_MESSAGE: ["You are all done! Hope you feel as great as me! Did you enjoy this class?", "You did it! What a great accomplishment. Did you like this class?"],
-      TEACH_CLASS_DONE_REPROMPT: "Was this class fun? Say yes for this class to be tracked on the A Lot of Pilates activities calendar.",
-      TEACH_CLASS_STANDARD_CARD_MESSAGE: "This is your pilates series of exercises for today. Enjoy your class.",
-      HELP_TEXT_NOTSTARTED_CLASS_MESSAGE: "If you are not familiar with Pilates exercises visit a lot of pilates dot com. There you can take video instructed classes to become comfortable with these exercises. Do you want to give it a try? ",
-      HELP_TEXT_DEFAULT_STATE_MESSAGE: "Sorry that you are having trouble with this class. At a lot of pilates dot com you can take video instructed classes to become comfortable with these exercises. Do you want to start a new class with me? ",
-      HELP_TEXT_CARD_TITLE: 'Pilates help',
-      STOP_UNSTARTED_CLASS_MESSAGE: "Ok. Hope you find a better time to start the class. Be sure to visit ALotOfPilates.com for pilates classes. Goodbye!",
-      FINISHED_WORKOUT_MESSAGE: "That is awesome. Now relax and enjoy the moment. Goodbye!",
-      CANCEL_CLASS_MESSAGE: "It is ok that you could not finish the class today. Visit ALotOfPilates.com for other pilates classes. Goodbye!",
-      NOT_A_FUN_CLASS_MESSAGE: "Sorry. How about you try another class next time. Visit ALotOfPilates.com for other pilates classes as well. Goodbye!",
-      NO_HELP_MESSAGE: "Sorry that you had a hard time with this class. Visit ALotOfPilates.com for other pilates classes, there are plenty to choose from. Good-bye",
-      CLASSES_TEXT: ' classes ',
-      CLASS_TEXT: ' class ',
-      FINISH_ENCOURAGEMENT: "Good job finishing a Pilates class. Visit ALotOfPilates.com for many more pilates classes. Goodbye!",
+      "Quando estiver prontra, diga inicie aula para começar. Posso começar?",
+      STARTOVER_MESSAGE: "Vamos iniciar a aula de pilate?",
+      STARTOVER_REPROMPT: "Você gostaria de inciar a aula de pilates agora?",    
+      GET_START_EXERCISE_MESSAGE: ["Pronta no mat para o %s", "Vamos começar com  %s"],
+      GET_NEXT_EXERCISE_MESSAGE: ["O próximo exercício é  %s", "Moving on to the %s", "Next is  %s"],
+      GET_START_EXERCISE_DEFAULT_MESSAGE:"Pronta com o mat no chão para começar?",
+      GET_NEXT_EXERCISE_DEFAULT_MESSAGE: "Vamos fazer o próximo exercício",
+      TEACH_CLASS_DONE_MESSAGE: "Terminamos a aula. Parabéns! Espero que você esteja se sentindo tão bem quanto eu. Você gostou da aula?",
+      TEACH_CLASS_DONE_REPROMPT: "Você gostou dessa aula? Se voce gostou eu posso adicionar isso ao seu calendário de progresso no ALotofPilates.",
+      TEACH_CLASS_STANDARD_CARD_MESSAGE: "Essa é a sua sequência de exercícios para a aula de hoje. Aproveite!",
+      HELP_TEXT_NOTSTARTED_CLASS_MESSAGE: "Se você gostaria de aprendar mais sobre os exercícios de pilates, por favor visite Alotofpilates.com. Lá existem várias outras aulas disponível para você aprendar mais. Você gostaria de tentar fazer aula comigo? ",
+      HELP_TEXT_DEFAULT_STATE_MESSAGE: "Que pena que você não esta conseguindo acompanhar os exercícios. No side alotofpilates.com, você pode assistir videos dessa aula para aprendar mais. Você gostaria de tentar novamente comigo? ",
+      HELP_TEXT_CARD_TITLE: 'Pilates Ajuda',
+      STOP_UNSTARTED_CLASS_MESSAGE: "Tudo bem. Espero que você ache uma melhor ocasião para fazer essa aula. Visita ALotOfPilates.com para aulas também. Tchauzinho!",
+      FINISHED_WORKOUT_MESSAGE: "Que maravilha! Agora você pode relaxar e curtir esse momento. Você merece. Tchau!",
+      CANCEL_CLASS_MESSAGE: "Tranquilo que você não pode terminar a aula hoje. Visite o site ALotOfPilates.com para varias aulas. Tchau!",
+      NOT_A_FUN_CLASS_MESSAGE: "Desculpa, mas que tal visitar ALotOfPilates.com. Existem varias aulas lá. Tchau!",
+      NO_HELP_MESSAGE: "Uma pena que não da para continuar. Da uma olhadinha no ALotOfPilates.com. Você tem accesso a varios outras aulas de pilates. Divirta-se!",
+      CLASSES_TEXT: ' aulas ',
+      CLASS_TEXT: ' aula ',
+      FINISH_ENCOURAGEMENT: "Parabéns por completar a aula! Visite ALotOfPilates.com várias outras aulas. Tchau!",
       TRACK_DISPLAY_CARD_TITLE: 'Pilates class activity',
       TRACK_DISPLAY_CARD_MESSAGE: " You have taken %d %s in %d. \r\nKeep track of your progress per month. \r\n%s\n \nVisit ALotOfPilates.com for many more classes and tracking calendar.",ACCOUNT_SETUP_ERROR_MESSAGE: 'Você deve criar uma conta em ALotOfPilates.com para comecar a aula. Por favor, use o applicativo da Alexa para conectar a sua conta da Amazon com a sua conta do ALotOfPilates.',
       GENERIC_MESSAGE: "I had a hard time understanding you. Say Start Pilates class or visit ALotOfPilates to take a class.",
